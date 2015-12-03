0332e153f690163158a995225fcee4b87a3ce633
joaosantos15 <jrmsantos15@gmail.com>
2015-12-03 23:56:36 +0000

    cenas

--- client/flush_to_proxy.sh
+++ client/flush_to_proxy.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR}
 python sleeper.py
-scp operations/results/connection_log.py pi1@192.168.2.100:/home/proxy/raw_data/pi1
-scp operations/results/speed_log.py pi1@192.168.2.100:/home/proxy/raw_data/pi1
+scp operations/results/connection_log.py pi1@192.168.2.104:/home/proxy/raw_data/pi1
+scp operations/results/speed_log.py pi1@192.168.2.104:/home/proxy/raw_data/pi1
